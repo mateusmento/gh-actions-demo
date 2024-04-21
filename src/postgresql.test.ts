e029b0c0396bdb04f0f53fe32cb475290cff8180
Mateus Sarmento <omateusmento@gmail.com>
2024-04-21 20:42:18 -0300

    Test persistance on postgresql testcontainer

--- src/postgresql.test.ts
+++ src/postgresql.test.ts
@@ -1,7 +1,9 @@
 import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import { Column, DataSource, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 describe("PostgreSQL testcontainers demo", () => {
   let database: StartedPostgreSqlContainer;
+  let dataSource: DataSource;
 
   beforeEach(async () => {
     database = await new PostgreSqlContainer("postgres:14")
@@ -9,9 +11,40 @@ describe("PostgreSQL testcontainers demo", () => {
       .withUsername("demo")
       .withPassword("demo")
       .start();
+
+    dataSource = new DataSource({
+      type: "postgres",
+      host: database.getHost(),
+      port: database.getPort(),
+      database: database.getDatabase(),
+      username: database.getUsername(),
+      password: database.getPassword(),
+      logging: true,
+      logger: 'advanced-console',
+      synchronize: true,
+      entities: [Issue]
+    })
+
+    await dataSource.initialize();
   });
 
   afterEach(async () => {
+    await dataSource.destroy();
     await database.stop();
+  });
+
+  @Entity()
+  class Issue {
+    @PrimaryGeneratedColumn()
+    id!: number;
+    @Column()
+    title!: string;
+  }
+
+  it("should persist an issue", async () => {
+    const issueRepo = dataSource.getRepository(Issue);
+    const ISSUE_TITLE = "Create issue";
+    const issue = await issueRepo.save({ title: ISSUE_TITLE });
+    expect(issue.title).toBe(ISSUE_TITLE);
   });
 });
